625dfb2c70ae2591145d019241b8236443bbeaba
deploy <deploy@126ae6b6-1cb0-44cf-83d6-85411bde8332>
2007-06-13 15:04:35 +0000

    Branch for NHBA
    git-svn-id: svn+ssh://svn.hellovenado.com/var/svn/tribe_cms_extensions/directory@44 126ae6b6-1cb0-44cf-83d6-85411bde8332

---
+++ branches/nhba/app/controllers/directory_orgs_controller.rb
@@ -0,0 +1,50 @@
+class DirectoryOrgsController < ApplicationController
+  # layout "main"
+  # no_login_required
+  
+  def index  
+    @organizations = DirectoryOrg.find(:all, :order => "name")
+  end
+
+  def show 
+    @organization = DirectoryOrg.find(params[:id])
+  end
+  
+  def edit
+    @organization = DirectoryOrg.find(params[:id])
+  end
+
+  def update
+    @organization = DirectoryOrg.find(params[:id])
+    if @organization.update_attributes(params[:directory_org]) and @organization.geocode
+      redirect_to directory_orgs_url
+    else
+      flash[:notice] = ""
+      @organization.errors.each_full { |e| flash[:notice] << e << ". " }
+      render :action => "edit"
+    end
+  end
+  
+  def new
+    @organization = DirectoryOrg.new
+  end
+  
+  def create
+    @organization = DirectoryOrg.new(params[:directory_org])    
+    if @organization.save and @organization.geocode
+      flash[:notice] = 'Organization was successfully created.'        
+      redirect_to directory_orgs_url
+    else
+      flash[:notice] = ""
+      @organization.errors.each_full { |e| flash[:notice] << e << ". " }
+      render :action => "new"
+    end
+  end
+
+  def destroy
+    @organization = DirectoryOrg.find(params[:id])
+    @organization.destroy
+    redirect_to directory_orgs_url
+  end
+
+end
---
+++ branches/nhba/directory_extension.rb
@@ -0,0 +1,21 @@
+class DirectoryExtension < Radiant::Extension
+  version "1.0"
+  description "Radiant Organization Directory Extension."
+  url "http://www.fn-group.com"
+
+  define_routes do |map|
+    map.resources :directory_orgs, :path_prefix => "/admin"
+  end
+  
+  def activate
+    Radiant::Config['directory.google_map_key'] = "ABQIAAAAKsmqeCs1JL6iKytv5zPbWRRkAWczaLFjKT11Xwc79OTdW8lxTRQj-CVbOBChwHcHX51DsffpGXAr4w"
+    admin.tabs.add "Directory", "/admin/directory_orgs", :after => "Layouts", :visibility => [:all]
+    DirectoryTags
+  end
+  
+  def deactivate
+    Radiant::Config['directory.google_map_key'] = nil
+    admin.tabs.remove "Directory"
+  end
+    
+end
