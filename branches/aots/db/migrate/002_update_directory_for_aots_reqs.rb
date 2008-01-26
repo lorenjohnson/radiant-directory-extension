57e6835ca720334e13c9f0634551aca163d49f6c
deploy <deploy@126ae6b6-1cb0-44cf-83d6-85411bde8332>
2008-01-26 21:05:38 +0000

    Fixed Geocoding to accept only city, state. Updated edit and new to better organize AOTS station attributes
    git-svn-id: svn+ssh://svn.hellovenado.com/var/svn/tribe_cms_extensions/directory@66 126ae6b6-1cb0-44cf-83d6-85411bde8332

--- branches/aots/db/migrate/002_update_directory_for_aots_reqs.rb
+++ branches/aots/db/migrate/002_update_directory_for_aots_reqs.rb
@@ -2,7 +2,7 @@ class UpdateDirectoryForAotsReqs < ActiveRecord::Migration
   def self.up
     add_column :organizations, :dial, :string
     add_column :organizations, :airtime, :string
-    add_column :organizations, :webstream, :string
+    add_column :organizations, :webstream, :boolean, :default => false
   end
 
   def self.down
