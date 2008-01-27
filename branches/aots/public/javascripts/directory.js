a44d9ff71c56d9397e135a08fd0bbcf092542169
deploy <deploy@126ae6b6-1cb0-44cf-83d6-85411bde8332>
2008-01-27 23:07:17 +0000

    reset map center to default (zoomed-out and center on USA)
    git-svn-id: svn+ssh://svn.hellovenado.com/var/svn/tribe_cms_extensions/directory@77 126ae6b6-1cb0-44cf-83d6-85411bde8332

--- branches/aots/public/javascripts/directory.js
+++ branches/aots/public/javascripts/directory.js
@@ -47,7 +47,7 @@ function map_load() {
 
   	map.addControl(new GSmallMapControl());
   	map.addControl(new GMapTypeControl());
-    map.setCenter(new GLatLng(45.5313220000000029585862648673355579376220703125, -122.693084), 14);
+    // map.setCenter(new GLatLng(45.5313220000000029585862648673355579376220703125, -122.693084), 5);
 	if(search_address) {
 		map_address_add(search_address,"Your address:<br />" + search_address);
 	}
